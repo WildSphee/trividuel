d461c1c2ae189f0bddb672b37c798b3b97fa781d
eryuanren <eryuanren@gmail.com>
2025-05-04 13:10:24 +0000

    remove unused loader nad icon

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -6,11 +6,5 @@ export default function Loader() {
       <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
       <p className="mt-4 text-gray-700 text-lg font-medium">Loading...</p>
     </div>
-
-    // Different loading animation, can decide later
-//     <div className="relative w-16 h-16">
-//         <div className="absolute inset-0 rounded-full border-4 border-blue-500 animate-ping"></div>
-//         <div className="absolute inset-0 rounded-full border-4 border-blue-500"></div>
-//   </div>
-  );
+  )
 }
