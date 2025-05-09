882543fd629bfd2c03ca4868b25466d097a643dc
eryuanren <eryuanren@gmail.com>
2025-05-09 16:16:38 +0000

    set up basic testing scripts with coverage reports

---
+++ backend/scripts/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ];
+then
+  echo "Defaulting to apply on './app'"
+  FPATH="./app"
+else
+ FPATH=$1
+fi
+
+# to run this `poetry add --dev pytest-cov coverage`
+pytest \
+    --cov-config scripts/.coveragerc \
+    --cov-report term-missing \
+    --cov --cov-report html \
+    --cov-context test \
+    --cov-fail-under 70 \
+    -v -r fEsX $FPATH
