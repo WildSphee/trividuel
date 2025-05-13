296a84ffe8338a689a7cee93f799af37da3ebd64
Reagan <rrr.chanhiulok@gmail.com>
2025-05-13 10:21:15 +0000

    update Loader style to pixelated

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function Loader() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
-      <p className="mt-4 text-gray-700 text-lg font-medium">Loading...</p>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {/* Pixelated spinning loader */}
+      <div className="relative w-20 h-20 bg-blue-500 border-[0.5rem] border-black box-shadow-pixelated animate-spin pixel-loader"></div>
+
+      {/* Pixelated loading text */}
+      <p className="font-block mt-4 text-black text-[2rem] font-bold box-shadow-pixelated-blocky px-4 py-2">
+        LOADING...
+      </p>
     </div>
-  )
+  );
 }
