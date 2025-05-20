b70c2122bc701d6e672e14132efbb1393e7e18a7
Reagan <rrr.chanhiulok@gmail.com>
2025-05-20 08:51:54 +0000

    make screen flexible with cramp for flexible UI

--- frontend/src/components/ChoiceButton.jsx
+++ frontend/src/components/ChoiceButton.jsx
@@ -1,10 +1,14 @@
+
 export default function ChoiceButton({ text, onClick, disabled, color, hoverColor }) {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`font-comic text-gray-700 pixel-choice-button w-full bg-${color}-500 hover:bg-${hoverColor}-400 
-        disabled:opacity-50 transition-all text-[1.25rem] py-[3rem] z-40`}
+      className={`font-comic text-gray-700 pixel-choice-button w-full bg-${color}-500 hover:bg-${hoverColor}-400 disabled:opacity-50 transition-all z-40`}
+      style={{
+        padding: "clamp(0.5rem, 4vh, 2rem) 0",
+        fontSize: "clamp(1rem, 2.5vw, 1.25rem)",
+      }}
     >
       {text}
     </button>
